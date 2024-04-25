03bb9f9c8fd68a0f1005b7c8f9186e7959db5432
Philip <philipjomer8@gmail.com>
2024-04-25 10:29:47 +0800

    first

---
+++ src/Example.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import ImgageGallary from "react-image-gallary"
+
+export default function Example() {
+  return (
+    <div>
+      <h1>Example</h1>
+      <p>
+        Edit <code>src/example.jsx</code> and save to test HMR
+      </p>
+      <ImageGallary />
+    </div>
+  )
+}
